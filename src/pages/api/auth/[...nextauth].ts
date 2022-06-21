b8a09009662e638f1fc04a9a836fba85e7625af4
Wallace <wallace_wolv@hotmail.com>
2022-06-21 00:29:02 -0300

    feat: Salvando usuário no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,21 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
